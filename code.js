c35d83780f769a2837dc51efdf9861c82a554f0e
Hamza Aldolaay <haldolaay@gmail.com>
2018-12-09 22:01:10 -0800

    Fibonacci

--- code.js
+++ code.js
@@ -1,22 +1,14 @@
 
-var x =1;
-function print(){
-    
-    if(x>0){
-        x--;
-        alert('hi');
-        print()
-        
-       
-    }
-
-}
-print();
-var tryingToPrint = document.getElementsByClassName("input").textContent;
-var newHtml = `<div>${tryingToPrint}<div>`;
-
-document.querySelector('#button').addEventListener('click',function(){
-    document.getElementById('div').insertAdjacentHTML("beforeend",newHtml);
-})
-
+// checking where to add a num to an array
+function IndexNum(arr, num) {
+    arr.push(num);
+    arr.sort(function(a, b){return a-b});
+    return arr.indexOf(num);
+};
+// Fibonacci
+function fibonacci(num) {
+    if (num <= 1) return 1;
+  
+    return fibonacci(num - 1) + fibonacci(num - 2);
+  }
 
