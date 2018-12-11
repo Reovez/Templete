1782f0dfda839bfe4db5e6e23f588b680600bec6
Hamza Aldolaay <haldolaay@gmail.com>
2018-12-10 23:30:49 -0800

    Finding a value

--- code.js
+++ code.js
@@ -11,4 +11,13 @@ function fibonacci(num) {
   
     return fibonacci(num - 1) + fibonacci(num - 2);
   }
-
+// finding a value
+function find(arr) {
+    var test = arr[1].toLowerCase();
+    var target = arr[0].toLowerCase();
+    for (var i=0;i<test.length;i++) {
+      if (target.indexOf(test[i]) < 0)
+        return false;
+    }
+    return true;
+   }
